43a67cb3512e0caaded3798f8ce0c7d4718166e2
vinnyvilasboa <vinnycesca@gmail.com>
2022-01-27 10:59:03 -0500

    Header, reactstrap, basics

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "19b9376b2045fe11"; }
+/******/ 		__webpack_require__.h = function() { return "53fde3e93b667990"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ pages/_app.js
@@ -0,0 +1,15 @@
+// pages/_app.js
+import Head from 'next/head'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
