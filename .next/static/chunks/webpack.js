12322a7e2a3cd15206bd0980a325413bc798e8be
vinnyvilasboa <vinnycesca@gmail.com>
2022-01-29 21:22:39 -0500

    final touches

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "67515b304f5ebac5"; }
+/******/ 		__webpack_require__.h = function() { return "c2f9be762f528031"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
